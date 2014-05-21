abb2ac315181876c89301a7804fcf40ab52f4f64
dimolto <aotokage52@gmail.com>
2014-05-22 07:39:15 +0900

    Added array.php

---
+++ tutorial/array.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ *Array
+ *array(2) {
+ *  ["foo"]=>
+ *  string(3) "bar"
+ *  ["bar"]=>
+ *  string(4) "hoge"
+ *}
+ */
+$numArray = array(
+	"foo" => "bar",
+	"bar" => "hoge",
+);
+print $numArray . "\n";
+var_dump($numArray);
+?>
