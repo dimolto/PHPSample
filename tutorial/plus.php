f17bc1ccd321b0260329398ce0cc422e9dd0bd1a
Oshiro Nobuyasu <nobuyasu@Oshiros-MacBook-Pro.local>
2014-05-21 23:53:43 +0900

    Added plus.php

---
+++ tutorial/plus.php
@@ -0,0 +1,13 @@
+<?php
+$x = 5;
+$y = 10;
+print $x + $y . "\n";
+$c = $x + $y;
+echo "$x + $y = {$c}\n";
+
+$number_1 = 3;
+$number_2 = 2;
+$answer = $number_1 + $number_2;
+echo "{$answer}\n"
+
+?>
